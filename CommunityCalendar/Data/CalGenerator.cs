1075ae4bd19143e71cdf0cb3dba019aefcf6e6e7
NTMTO <55151864+NTMTO@users.noreply.github.com>
2023-11-09 05:55:35 -0600

    Initial

---
+++ CommunityCalendar/Data/CalGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System;
+namespace CommunityCalendar.Data
+{
+    public class CalGenerator
+    {
+        //Props
+
+        //Constructor (Not Needed?)
+
+        //Methods
+
+        public List<DateTime> Calculate(DateTime ChosenMonth)
+        {
+            DateTime workingDate = DateTime.Now;
+            //To ensure the datetime is in the future..
+            while (workingDate.Month != ChosenMonth.Month)
+            {
+                workingDate.AddMonths(1);
+            }
+            workingDate = new DateTime(workingDate.Year, workingDate.Month, 1);
+            DateTime endDate = workingDate.AddMonths(1);
+            List<DateTime> workingList = new List<DateTime>();
+            while (workingDate < endDate)
+            {
+                workingList.Add(workingDate);
+                workingDate = workingDate.AddDays(1);
+            }
+            return workingList;
+         }
+
+    }
+}
+
